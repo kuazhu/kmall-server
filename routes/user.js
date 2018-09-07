01ac8823d94594e2800e89ac311aa3ea4ad3b790
TomChen <76876201@qq.com>
2018-09-07 17:28:01 +0800

    user model

--- routes/user.js
+++ routes/user.js
@@ -2,33 +2,13 @@
 * @Author: Tom
 * @Date:   2018-08-06 09:23:30
 * @Last Modified by:   TomChen
-* @Last Modified time: 2018-08-27 10:53:59
+* @Last Modified time: 2018-09-07 16:36:55
 */
 const Router = require('express').Router;
 const UserModel = require('../models/user.js');
 const hmac = require('../util/hmac.js')
 
 const router = Router();
-/*
-router.get('/init',(req,res)=>{
-	const users = [];
-
-	for(let i = 0;i<100;i++){
-		users.push({
-			username:'test'+i,
-			password:hmac('test'+i),
-			isAdmin:false,
-			phone:'13681822'+i,
-			email:'test'+i+'@kuazhu.com'			
-		})
-	}
-	UserModel.create(users)
-	.then((result)=>{
-		res.send('ok');
-	})
-
-})
-*/
 
 //注册用户
 router.post("/register",(req,res)=>{
@@ -43,21 +23,22 @@ router.post("/register",(req,res)=>{
 	.findOne({username:body.username})
 	.then((user)=>{
 		if(user){//已经有该用户
-			 result.code = 10;
+			 result.code = 1;
 			 result.message = '用户已存在'
 			 res.json(result);
 		}else{
 			//插入数据到数据库
 			new UserModel({
 				username:body.username,
+				phone:body.phone,
+				email:body.email,
 				password:hmac(body.password),
-				// isAdmin:true
 			})
 			.save((err,newUser)=>{
 				if(!err){//插入成功
 					res.json(result)
 				}else{
-					result.code = 10;
+					result.code = 1;
 					result.message = '注册失败'
 					res.json(result);					
 				}
@@ -75,28 +56,17 @@ router.post("/login",(req,res)=>{
 		message:''
 	}
 	UserModel
-	.findOne({username:body.username,password:hmac(body.password)})
+	.findOne({username:body.username,password:hmac(body.password),isAdmin:false})
 	.then((user)=>{
 		if(user){//登录成功
-			 /*	
-			 result.data = {
-			 	_id:user._id,
-			 	username:user.username,
-			 	isAdmin:user.isAdmin
-			 }
-			 */
-			 //设置cookie->返回时前端页面就会有设置的cookie
-			 //req.cookies.set('userInfo',JSON.stringify(result.data))
-			 
 			 req.session.userInfo = {
 			 	_id:user._id,
 			 	username:user.username,
 			 	isAdmin:user.isAdmin
 			 }
-
 			 res.json(result);
 		}else{
-			result.code = 10;
+			result.code = 1;
 			result.message = '用户名和密码错误'
 			res.json(result);
 		}
@@ -104,19 +74,95 @@ router.post("/login",(req,res)=>{
 
 })
 
+
+router.get("/username",(req,res)=>{
+	if(req.userInfo._id){
+		res.json({
+			code:0,
+			data:{
+				username:req.userInfo.username
+			}
+		})
+	}else{
+		res.json({
+			code:1
+		});
+	}
+});
+
+
+router.get("/checkUsername",(req,res)=>{
+	let username = req.query.username;
+	UserModel
+	.findOne({username:username})
+	.then((user)=>{
+		if(user){
+			res.json({
+				code:1,
+				message:'用户名已存在'
+			})
+		}else{
+			res.json({
+				code:0,
+			})
+		}
+	})
+});
+
+
 //退出
 router.get('/logout',(req,res)=>{
 	let result  = {
 		code:0,// 0 代表成功 
 		message:''
 	}
-	/*	
-	req.cookies.set('userInfo',null);
-	*/
 	req.session.destroy();
 
 	res.json(result);
 
+})
+
+//权限控制
+router.use((req,res,next)=>{
+	if(req.userInfo._id){
+		next()
+	}else{
+		res.json({
+			code:10
+		})
+	}
+})
+
+router.get("/userInfo",(req,res)=>{
+	if(req.userInfo._id){
+		UserModel.findById(req.userInfo._id,"username phone email")
+		.then(user=>{
+			res.json({
+				code:0,
+				data:user
+			})
+		})
+	}else{
+		res.json({
+			code:1
+		});
+	}
+});
+
+router.put("/updatePassword",(req,res)=>{
+	UserModel.update({_id:req.userInfo._id},{password:hmac(req.body.password)})
+	.then(raw=>{
+		res.json({
+			code:0,
+			message:'更新密码成功'
+		})
+	})
+	.catch(e=>{
+		res.json({
+			code:1,
+			message:'更新密码失败'
+		})
+	})
 })
 
 module.exports = router;
